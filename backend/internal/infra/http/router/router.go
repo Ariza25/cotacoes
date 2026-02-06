5d9b564cd803527961121e9c802472ef2c031bca
Matheus Ariza <mjsa.sanches25@gmail.com>
2026-02-06 16:27:21 -0300

    path

--- backend/internal/infra/http/router/router.go
+++ backend/internal/infra/http/router/router.go
@@ -16,7 +16,7 @@ func SetupRouter(h Handlers) *gin.Engine {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://frontend-cotacoes.onrender.com"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
